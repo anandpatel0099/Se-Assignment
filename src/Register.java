72b991ef52e7fb2b053b998e95f2da63f7174603
anandpatel0099 <you@example.com>
2017-09-24 21:04:47 -0400

    first change

---
+++ src/Register.java
@@ -0,0 +1,62 @@
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Register {
+	
+	private String Username;
+	private String Password;
+	private String Firstname;
+	private String Lastname;
+	private String Address;
+	private String Phonenumber;
+	private String Email;
+	
+	
+	public String getUsername() {
+		return Username;
+	}
+	public void setUsername(String username) {
+		Username = username;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public String getFirstname() {
+		return Firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.Firstname = firstname;
+	}
+	public String getLastname() {
+		return Lastname;
+	}
+	public void setLastname(String lastname) {
+		this.Lastname = lastname;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		this.Address = address;
+	}
+	public String getPhonenumber() {
+		return Phonenumber;
+	}
+	public void setPhonenumber(String phonenumber) {
+		this.Phonenumber = phonenumber;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		this.Email = email;
+	}
+
+	
+	
+	
+
+}
